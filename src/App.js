501f74a62c6a4c72e55f44db62845ec60fc33bc5
Faktor 10 <oreillyross@gmail.com>
2022-05-06 14:45:16 +0200

    change text color

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
       <h1 className=" p-2 basis-3/4 text-red-800 hover:text-green-800 hover: cursor-none">
         Hello CodeSandbox
       </h1>
-      <h2 className="font-sans tracking-wide">
+      <h2 className="p-3 text-blue-500 font-sans tracking-wide">
         Start editing to see some magic happen!
       </h2>
     </div>
