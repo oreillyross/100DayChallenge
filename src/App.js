202d899a53a6aa4acce398b56ba5ef22564e87b7
Faktor 10 <oreillyross@gmail.com>
2022-05-06 14:14:46 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import "./tailwind-pre-build.css";
+
+export default function App() {
+  return (
+    <div className="bg-slate-200 flex flex-row">
+      <h1 className=" p-2 basis-3/4 text-red-800 hover:text-green-800 hover: cursor-none">
+        Hello CodeSandbox
+      </h1>
+      <h2 className="font-sans tracking-wide">
+        Start editing to see some magic happen!
+      </h2>
+    </div>
+  );
+}
