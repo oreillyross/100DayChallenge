4805b05a8cb2251623e7c6c24d72218d30b54b61
Ross O'Reilly <oreillyross@gmail.com>
2022-05-07 11:05:49 +0200

    move to typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+export default function App() {
+  return (
+    <div className="flex max-h-64 justify-center h-screen bg-gradient-to-tr from-slate-100 to-slate-300">
+      <h1 className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+        100 Days of Coding Challenge 
+      </h1>
+      
+    </div>
+  );
+}
