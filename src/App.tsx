761cf92b905b5846604905051fcd7abcdeaa5bbe
Ross O'Reilly <oreillyross@gmail.com>
2022-05-10 16:25:54 +0200

    add motivation and key skills flex items

--- src/App.tsx
+++ src/App.tsx
@@ -42,23 +42,45 @@ export default function App() {
         <div
           className="container flex flex-col-reverse 
                           md:flex-row 
-                          items-center px-6 mx-auto 
-                          mt-10 space-y-0 md:space-y-0"
+                           px-6 mx-auto 
+                          mt-10 space-y-0 md:space-y-0 items-center"
         >
           <div className="flex flex-col  mb-32 space-y-12 md:w-1/2">
-            <h1 className="max-w-md text-4xl font-bold  md:textleft md:text-5xl">
+            <h1 className="p-10 text-center max-w-md text-3xl font-bold  md:text-left md:text-5xl block">
               Hi, 👋 I am Ross, your environmentally friendly developer
             </h1>
-            <div>
-              Full stack software engineer an expert in coding in Typescript,
+            <div className="max-w-sm text-center text-slate-900 md:text-left text-lg pr-12 ">
+              Full stack software engineer expert in coding in Typescript,
               Javascript, and using React, NextJS, GraphQL, Serverless, HTML and
               CSS
             </div>
+            <div className="flex justify-center md:justify-start">
+              <a
+                href="#"
+                className=" p-2 px-3 pt-2 text-white bg-gray-800 rounded-full
+                     baseline border-2
+                   hover:border-gray-900 
+                     hover:text-slate-900 
+                     hover:bg-white"
+              >
+                My portfolio
+              </a>
+            </div>
           </div>
-          <img src="/img/freelancer.svg" alt="" />
+          <div className=" pb-10">
+            <img src="/img/freelancer.svg" alt="" />
+          </div>
         </div>
       </section>
       {/* Hero section end */}
+      {/* features section start*/}
+      <section id="features">
+        <div className="container flex flex-col px-4 mx-auto mt-10 space-y-12 md:space-y-0">
+          <div>{/* motivation summary */}</div>
+          <div>{/* key skills */}</div>
+        </div>
+      </section>
+      {/* features section end */}
     </>
   );
 }
