cab2750cc5c8ac81cd11188c1a2aacdc7a398bb3
Ross O'Reilly <oreillyross@gmail.com>
2022-05-15 19:03:15 +0200

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
-import { BrowserRouter, Link } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Content from "./Content";
+import About from "./routes/About";
+import Engage from "./routes/Engage";
+import Projects from "./routes/Projects";
 
 
 export default function App() {
@@ -10,7 +14,7 @@ export default function App() {
         <div className="flex items-center justify-between">
           <div className="pt-2">
             {/* Logo */}
-            <img src="/img/nature.svg" alt="" width="100px" height="100px" />
+            <a href="/"><img src="/img/nature.svg" alt="" width="100px" height="100px" /></a>
           </div>
           <div className="hidden md:flex space-x-6 text-white">
             <Link to="/about" className="hover:text-slate-400">
@@ -40,70 +44,13 @@ export default function App() {
       </nav>
       {/* navbar section end */}
       {/* Hero section start */}
-      <section id="hero">
-        <div
-          className="container flex flex-col-reverse 
-                          md:flex-row 
-                           px-6 mx-auto 
-                          mt-10 space-y-0 md:space-y-0 items-center"
-        >
-          <div className="flex flex-col  mb-32 space-y-12 md:w-1/2">
-            <h1 className="p-10 text-center max-w-md text-3xl font-bold  md:text-left md:text-5xl block">
-              Hi, 👋 I am Ross, your environmentally friendly developer
-            </h1>
-            <div className="max-w-sm text-center text-slate-900 md:text-left text-lg pr-12 ">
-              Full stack software engineer expert in coding in Typescript,
-              Javascript, and using React, NextJS, GraphQL, Serverless, HTML and
-              CSS
-            </div>
-            <div className="flex justify-center md:justify-start">
-              <a
-                href="#"
-                className=" p-2 px-3 pt-2 text-white bg-gray-800 rounded-full
-                     baseline border-2
-                   hover:border-gray-900 
-                     hover:text-slate-900 
-                     hover:bg-white"
-              >
-                My portfolio
-              </a>
-            </div>
-          </div>
-          <div className=" pb-10">
-            <img src="/img/freelancer.svg" alt="" />
-          </div>
-        </div>
-      </section>
-      {/* Hero section end */}
-      {/* features section start*/}
-      <section id="features">
-        <div className="container flex flex-col md:flex-row px-4 mx-auto mt-10 space-y-12 md:space-y-0">
-          <div className="flex flex-col space-y-12 md:w-1/2">
-            {/* motivation summary */}
-            <h2 className="max-w-md text-4xl text-center md:text-left font-bold">
-              What drives me!
-            </h2>
-            <p className="max-w-sm text-center md:text-left text-slate-800">
-              I am strongly driven by TK ideas which suggest that we live in an
-              infinitely bountiful world. Mention Peter Thiel etc,
-            </p>
-          </div>
-          <div className="flex flex-col space-y-12 md:w-1/2">
-            {/* key skills */}
-            <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
-              <div className="rounded-l-full bg-red-300 md:bg-transparent" >
-
-                <div className="flex items-center space-x-2">
-                  <div className="px-4 py-2 text-white bg-red-500 md:py-1 rounded-full">
-                    01
-                  </div>
-                  <div className="h3 text-base font-bold">Constantly exploring functional programming paradigms</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Routes>
+        <Route path="/" element={<Content/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/engage" element={<Engage/>}/>
+      </Routes>
+      
       {/* features section end */}
     </BrowserRouter>
   );
---
+++ src/Content.tsx
@@ -0,0 +1,70 @@
+export default function Content() {
+	return (
+		<>
+		<section id="hero">
+        <div
+          className="container flex flex-col-reverse 
+                          md:flex-row 
+                           px-6 mx-auto 
+                          mt-10 space-y-0 md:space-y-0 items-center"
+        >
+          <div className="flex flex-col  mb-32 space-y-12 md:w-1/2">
+            <h1 className="p-10 text-center max-w-md text-3xl font-bold  md:text-left md:text-5xl block">
+              Hi, 👋 I am Ross, your environmentally friendly developer
+            </h1>
+            <div className="max-w-sm text-center text-slate-900 md:text-left text-lg pr-12 ">
+              Full stack software engineer expert in coding in Typescript,
+              Javascript, and using React, NextJS, GraphQL, Serverless, HTML and
+              CSS
+            </div>
+            <div className="flex justify-center md:justify-start">
+              <a
+                href="#"
+                className=" p-2 px-3 pt-2 text-white bg-gray-800 rounded-full
+                     baseline border-2
+                   hover:border-gray-900 
+                     hover:text-slate-900 
+                     hover:bg-white"
+              >
+                My portfolio
+              </a>
+            </div>
+          </div>
+          <div className=" pb-10">
+            <img src="/img/freelancer.svg" alt="" />
+          </div>
+        </div>
+      </section>
+      {/* Hero section end */}
+      {/* features section start*/}
+      <section id="features">
+        <div className="container flex flex-col md:flex-row px-4 mx-auto mt-10 space-y-12 md:space-y-0">
+          <div className="flex flex-col space-y-12 md:w-1/2">
+            {/* motivation summary */}
+            <h2 className="max-w-md text-4xl text-center md:text-left font-bold">
+              What drives me!
+            </h2>
+            <p className="max-w-sm text-center md:text-left text-slate-800">
+              I am strongly driven by TK ideas which suggest that we live in an
+              infinitely bountiful world. Mention Peter Thiel etc,
+            </p>
+          </div>
+          <div className="flex flex-col space-y-12 md:w-1/2">
+            {/* key skills */}
+            <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+              <div className="rounded-l-full bg-red-300 md:bg-transparent" >
+
+                <div className="flex items-center space-x-2">
+                  <div className="px-4 py-2 text-white bg-red-500 md:py-1 rounded-full">
+                    01
+                  </div>
+                  <div className="h3 text-base font-bold">Constantly exploring functional programming paradigms</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+		</>
+	)
+}
