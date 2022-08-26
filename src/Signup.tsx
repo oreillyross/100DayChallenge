0b9cdff8f1ed5ac7f53600aea2943be01b0d9d60
Faktor 10 <oreillyross@gmail.com>
2022-08-26 12:57:12 +0200

    change to github button

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -3,7 +3,7 @@ export default function Signup() {
     <section>
       <div>Sign up</div>
       <div>Choose one of the following signup methods</div>
-      <div>[Twitter button][Facebook button][Google button]</div>
+      <div>[Twitter button][Github button][Google button]</div>
       <div>Or sign up using your email address</div>
       <form>
         <input placeholder="email adress" />
